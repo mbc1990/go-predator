c778bac16e01dd15772a0df7b1e3fd68f659f70d
mbc <malcolmbalch@gmail.com>
2017-11-04 20:38:06 -0400

    Download images to work directory

--- predator.go
+++ predator.go
@@ -1,15 +1,40 @@
 package main
 
 import "fmt"
+import "io"
+import "log"
+import "net/http"
+import "os"
+import "strings"
 
 type Predator struct {
 	TwitterClient *TwitterClient
 	Conf          *Configuration
 }
 
-// Downloads an image
-func (p *Predator) GetImage(url string) {
+// Downloads and deduplicates an image
+func (p *Predator) HandleImage(url string) {
+	fmt.Println("Downloading image from " + url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
 
+	// Get the filename
+	spl := strings.Split(url, "/")
+	fname := spl[len(spl)-1]
+
+	// Create the file and copy the response body into it
+	file, err := os.Create(p.Conf.UnclassifiedWorkDir + fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
 }
 
 // Entry point for a single run across all image sources
@@ -20,8 +45,7 @@ func (p *Predator) Run() {
 			medias := tweet.Entities.Media
 			for _, media := range medias {
 				url := media.Media_url
-				// TODO: Get image
-				fmt.Println(url)
+				p.HandleImage(url)
 			}
 		}
 	}
